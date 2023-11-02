166a09c2ce7e8a2357ee73119b880615e55e8d1e
sowmyakavali <51850438+sowmyakavali@users.noreply.github.com>
2023-11-02 23:34:05 +0530

    Update execute_hm_pipeline.py

--- heatmap/method2/execute_hm_pipeline.py
+++ heatmap/method2/execute_hm_pipeline.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     dp = DataPreparation( hm_height = 24, hm_width = 24,
                          ip_width = 96, ip_height=96,
                          sigma = 4, stride = 5, noOfkps=9)
-    csvPath = r"d:\Shoe_Tryon\kps-datafilter\Datasets\Datasets1to8\FinalDataset_96x96\RGBFinalData.csv"
+    csvPath = r"FinalDataset_96x96\RGBFinalData.csv"
 
     heatmaps, images = dp.getKeyPointData(csvPath)
 
@@ -21,4 +21,4 @@ if __name__ == "__main__":
     trainmodule.train(ims, hms, epochs=1000, batch_size=32)
 
     # test = Testing()
-    # test.testmodel(r"D:\Shoe_Tryon\Shoe_KP_HM_Training\shoesHM_V1.h5", "testkps")
+    # test.testmodel(r"HM_V1.h5", "testkps")
